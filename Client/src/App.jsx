196540d67c2b205e5ab112b5933ba706b64e9108
miryamgou <96470548+miryamgou@users.noreply.github.com>
2025-07-14 17:29:12 +0300

    communityPage

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AdminViewUserProfile from "./pages/AdminProfileUser";
+<<<<<<< Updated upstream
 // import Sidebar from "./pages/Sidebar";   
 
+=======
+import Sidebar from "./pages/Sidebar";   
+import UploadData from "./pages/UploadMemberData";
+import UserSearchComponent from "./pages/SearchPage";
+>>>>>>> Stashed changes
 import UserProfileForm from "./pages/UserProfileForm";
-// import UploadData from "./pages/UploadMemberData";
-// import Slider from "./pages/Slider";
+import CommunityMembersPage from "./pages/CommunityPage";
+
 
 function App() {
   return (
     <Router>
       <Routes>
+<<<<<<< Updated upstream
        <Route path="/" element={<UserProfileForm />} />
         {/* <Route path="/member/uploadData" element={<UploadData />} /> */}
          {/* <Route path="/" element={<AdminViewUserProfile userId={null} />} /> */}
         {/* <Route path="/member/id:/data/" element={<UserProfileForm />} /> */}
+=======
+
+       <Route path="/member/id:/data/" element={<UserProfileForm />} />
+        <Route path="/UserSearch" element={<UserSearchComponent />} />
+       <Route path="/manager/group/:idGroup" element={<CommunityMembersPage />} />
+        <Route path="/member" element={<UploadData />} />
+         <Route path="/sidebar" element={<Sidebar />} />
+        <Route path="/manager/members/:id" element={<AdminViewUserProfile />} />
+    
+>>>>>>> Stashed changes
       </Routes>
     </Router>
   );
