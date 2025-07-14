b0a7b53b1042b8fd39815ea313ba1e8dbb3efa43
Shira <shira.persky@gmail.com>
2025-07-14 20:12:02 +0300

    add function for the first page of the member who get linkedin url and return id member

--- Server/api_routing/upload.js
+++ Server/api_routing/upload.js
@@ -1,9 +1,22 @@
-import express from 'express';
+import { createMemberWithLinkedIn } from "../services/uploadService.js";
+
+import express from "express";
 const router = express.Router();
 
-// definition
+// POST create with linkedin_url
+router.post("/", async (req, res) => {
+  try {
+    const { linkedin_url } = req.body;
+    const newMember = await createMemberWithLinkedIn(linkedin_url);
+    res
+      .status(201)
+      .json({ id_community_member: newMember.id_community_member });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: err.message });
+  }
+});
 
-router.post('/', anotherFunc);
 
 
 export default router;
