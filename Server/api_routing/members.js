71059db5944332492657582e306a59f0a4adae1d
popper <popper@LAPTOP-MF8PAB5D>
2025-07-14 14:44:43 +0300

    import cors

--- Server/api_routing/members.js
+++ Server/api_routing/members.js
@@ -2,7 +2,14 @@ import express from 'express';
 const router = express.Router();
 
 // definition
-router.get('/', controllerFunc);
+router.get('/', (req,res)=>{
+    try{
+
+    }
+    catch(err) {
+res.status(400).send("[]")
+    }
+});
 
 router.get('/id:', anotherFunc);
 
--- Server/app.js
+++ Server/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 import communityMemberRoutes from './api_routing/community_member.router.js';
 import memberRoutes from './api_routing/members.router.js';
 import searchRoutes from './api_routing/search.router.js';
 import uploadRoutes from './api_routing/upload.router.js';
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.use('/member', communityMemberRoutes); 
 app.use('/manager/members', memberRoutes);
