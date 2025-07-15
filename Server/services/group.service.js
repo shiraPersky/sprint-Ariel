cf85f623b1c3b1c000cf38e081fa00b969e8d563
popper <popper@LAPTOP-MF8PAB5D>
2025-07-15 12:55:05 +0300

    get dy id group - service'

--- Server/services/group.service.js
+++ Server/services/group.service.js
@@ -1,9 +1,22 @@
 import { getAll } from '../dataLayer/group.data.js';
 
+import { getByIds } from '../dataLayer/groupMember.data.js';
+
 // Service function to get all groups
 export async function getAllGroups() {
   const groups = await getAll();
 
   // You can add more processing logic here if needed
   return groups;
+}
+
+
+
+// Service function to get members by group ID
+export async function getMembersByGroupId(id_group) {
+  if (!id_group) {
+    throw new Error('Group ID must be provided');
+  }
+  const members = await getByIds({ id_group });
+  return members;
 }
