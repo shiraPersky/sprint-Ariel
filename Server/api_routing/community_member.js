2f1cec0b9a1d32d9408a8dc245ee86b994fc8e6f
Shira <shira.persky@gmail.com>
2025-07-15 20:09:18 +0300

    fix bug in getMembersByGroupId function

--- Server/api_routing/community_member.js
+++ Server/api_routing/community_member.js
@@ -63,23 +63,5 @@ router.post("/linkedin", async (req, res) => {
 });
 
 
-    
 
-// router.put('/:id', async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const updateData = req.body;
-
-//     const updatedUser = await updateMemberById(id, updateData);
-
-//     if (!updatedUser) {
-//       return res.status(404).json({ message: 'User not found or update failed' });
-//     }
-
-//     res.json(updatedUser);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: 'Server error' });
-//   }
-// });
 export default router;
